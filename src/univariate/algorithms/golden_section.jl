d86f85f5234b3fb35cb334a8b545dae664536a40
lhnguyen-vn <54606566+lhnguyen-vn@users.noreply.github.com>
2020-09-09 21:17:24 +0000

    Add golden section search algorithm

---
+++ src/univariate/algorithms/golden_section.jl
@@ -0,0 +1,26 @@
+struct GoldenSection <: UnivariateAlgorithm end
+
+function _optimize(f, lower, upper, algorithm::GoldenSection; rel_tol, abs_tol, max_iter, kwargs...)
+    p = φ - 1
+    x = p * lower + (1 - p) * upper
+    yx = f(x)
+    converged = false
+    iter = 1
+    while iter < max_iter
+        x_tol = rel_tol * abs(x) + abs_tol
+        mid_point = (lower + upper) / 2
+        if abs(x - mid_point) <= 2x_tol - abs(upper - lower) / 2
+            converged = true
+            break
+        end
+        iter += 1
+        new_x = p * upper + (1 - p) * lower
+        new_yx = f(new_x)
+        if yx > new_yx
+            lower, x, yx = x, new_x, new_yx
+        else
+            lower, upper = new_x, lower
+        end
+    end
+    return UnivariateSolution(converged, iter, x, yx)
+end

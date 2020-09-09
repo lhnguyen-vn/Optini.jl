dd576e329b69f525c004069015b3c5a903dc2f4e
lhnguyen-vn <54606566+lhnguyen-vn@users.noreply.github.com>
2020-09-09 21:17:58 +0000

    Update module file

--- src/Optini.jl
+++ src/Optini.jl
@@ -1,3 +1,22 @@
 module Optini
 
+import MathOptInterface
+const MOI = MathOptInterface
+
+import Base.MathConstants: φ
+
+# Generic method types
+include("types.jl")
+
+# Univariate optimizers
+include("univariate/types.jl")
+include("univariate/utils.jl")
+include("univariate/optimize.jl")
+include("univariate/algorithms/fibonacci.jl")
+include("univariate/algorithms/golden_section.jl")
+include("univariate/algorithms/quadratic_fit.jl")
+
+export Fibonacci, GoldenSection, QuadraticFit
+export optimize
+
 end

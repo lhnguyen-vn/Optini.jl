d8a905274bb70475edbaca681ed0dcb950286f31
lhnguyen-vn <54606566+lhnguyen-vn@users.noreply.github.com>
2020-10-26 02:50:18 +0000

    Add docs for abstract type

--- src/types.jl
+++ src/types.jl
@@ -12,9 +12,26 @@ Abstract supertype of univariate optimization algorithms.
 """
 abstract type UnivariateAlgorithm <: AbstractAlgorithm end
 
+"""
+    MultivariateAlgorithm
+
+Abstract supertype of multivariate optimization algorithms.
+"""
 abstract type MultivariateAlgorithm <: AbstractAlgorithm end
 
+"""
+    FirstOrderAlgorithm
+
+Abstract supertype of multivariate optimization algorithms requiring first-order knowledge.
+"""
 abstract type FirstOrderAlgorithm <: MultivariateAlgorithm end
+
+"""
+    SecondOrderAlgorithm
+
+Abstract supertype of multivariate optimization algorithms requiring second-order knowledge.
+"""
+abstract type SecondOrderAlgorithm <: MultivariateAlgorithm end
 
 """
     Solution{Tx, Tf}

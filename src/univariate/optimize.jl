376fe8dc203d418aa5e06636f6be32a2ae62d637
lhnguyen-vn <54606566+lhnguyen-vn@users.noreply.github.com>
2020-12-07 07:16:35 +0000

    Shorten keyword arguments

--- src/univariate/optimize.jl
+++ src/univariate/optimize.jl
@@ -1,10 +1,10 @@
 function optimize(f::Function, lower::T, upper::T; 
         alg::UnivariateAlgorithm=GoldenSection(), 
-        rel_tol=sqrt(eps(T)), 
-        abs_tol=eps(T), 
-        max_iter::Integer=1_000) where {T<:AbstractFloat}
+        reltol=sqrt(eps(T)), 
+        abstol=eps(T), 
+        maxiter::Integer=1_000) where {T<:AbstractFloat}
     lower < upper || error("Variable lower bound has to be smaller than upper bound.")
-    _optimize(f, lower, upper, alg; rel_tol=T(rel_tol), abs_tol=T(abs_tol), max_iter)
+    _optimize(f, lower, upper, alg; reltol=T(reltol), abstol=T(abstol), maxiter)
 end
 
 """
@@ -14,9 +14,9 @@ Compute the minimizer of the function `f` using the bracket (`lower`, `upper`).
 
 # Keywords
 - `alg::UnivariateAlgorithm=GoldenSection()`: the algorithm choice
-- `rel_tol=sqrt(eps(T))`: relative tolerance
-- `abs_tol=eps(T)`: absolute tolerance
-- `max_iter::Integer=1_000`: maximum numer of iterations
+- `reltol=sqrt(eps(T))`: relative tolerance
+- `abstol=eps(T)`: absolute tolerance
+- `maxiter::Integer=1_000`: maximum numer of iterations
 """
 function optimize(f::Function, lower::Real, upper::Real; kwargs...)
     T = promote_type(typeof(lower/1), typeof(upper/1))
